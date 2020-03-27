b2eb2b993531194207a377af42d8e158fc476da5
vinay-techie <58105215+vinay-techie@users.noreply.github.com>
2020-03-27 11:55:16 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,38 @@
+var express = require('express')
+var app = express()
+const http= require('http').Server(app)
+app.use(express.static(__dirname))
+var server = http.listen(3000,() =>{
+	console.log(`${__dirname}`)
+})
+var io = require('socket.io')(http)
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.use((req,res,next)=>{
+     res.header("Access-Control-Allow-Origin","*");
+     res.header("Access-Control-Allow-Methods","GET,PUT,POST,DELETE")
+     res.header(
+          "Acess-Control-Allow-Headers",
+          "Content-Type"
+          );
+     next();
+          
+});
+var messages = []
+app.get('/vinay',(req,res)=>{
+     res.send(messages);
+   
+})
+app.post('/vinay',(req,res)=>{
+     console.log(req.body);
+     messages.push(req.body);
+     io.emit('message',req.body)
+     res.sendStatus(200);
+})
+io.on('connection',(socket)=>{
+     console.log('user connected');
+})
+
+
+
