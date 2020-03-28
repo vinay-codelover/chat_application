690748c953c15d488477d314b2a9b2a568fcfb2a
vinay-techie <58105215+vinay-techie@users.noreply.github.com>
2020-03-28 21:24:05 +0530

    Update server.js

--- root/server.js
+++ root/server.js
@@ -18,12 +18,19 @@ app.use((req,res,next)=>{
      next();
           
 });
+app.use((req, res, next) => {
+  if (req.originalUrl === '/favicon.ico') {
+    res.status(204).json({nope: true});
+  } else {
+    next();
+  }
+});
 var messages = []
 app.get('/vinay',(req,res)=>{
      res.send(messages);
    
 })
-app.get('/favicon.ico', (req, res) => res.status(204));
+
 app.post('/vinay',(req,res)=>{
      console.log(req.body);
      messages.push(req.body);
